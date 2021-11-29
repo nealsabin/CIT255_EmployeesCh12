6f9b1502d4f75995457ae69cc95c7e7e62d6ed0a
nealsabin <sabin7@mail.nmc.edu>
2021-11-28 20:26:41 -0500

    8.26

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        // Composite Primary key and foreign key for Department
+        public int DepartmentID { get; set; }
+        // Composite Primary key and foreign key for location
+        public int LocationID { get; set; }
+
+        // navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
